fb56b687e26b47eb1d8a187e7e04563fa605a041
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-04-26 15:54:58 +0300

    js add Api

---
+++ src/js/API.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+const URL = 'https://pixabay.com/api/';
+const KEY = '26979119-f1478b332cc81f8a5f9b5f4c9';
+
+export default class ServiceAPI {
+  constructor() {
+    this.options = {
+      params: {
+        key: `${KEY}`,
+        q: '',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+      },
+    };
+  }
+
+  async getPictures() {
+    try {
+      const response = await axios.get(`${URL}`, this.options);
+      await this.nextPage();
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async nextPage() {
+    this.options.params.page += 1;
+  }
+
+  resetPage() {
+    this.options.params.page = 1;
+  }
+
+  get searchQuery() {
+    return this.options.params.q;
+  }
+
+  set searchQuery(newQuery) {
+    this.options.params.q = newQuery;
+  }
+}
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  searchBtn: document.querySelector('[type=submit]'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
+};
